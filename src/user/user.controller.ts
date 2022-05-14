2ca86a10c186ed804f97b7602186757ff518aa0b
Yusuf Ganiyu <airscholar@gmail.com>
2022-05-14 18:38:36 +0100

    Added user controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator/get-user.decorator';
+import { JwtGuard } from 'src/auth/guard';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  @Get('me')
+  async getMe(@GetUser('id') userId: string) {
+    return {
+      msg: 'user info',
+      userId,
+    };
+  }
+}
